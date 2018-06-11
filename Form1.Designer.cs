eaa17f8b7fcf8f105949f53ab4e97c1042f9c15a
ciokos <Ciokos23>
2018-06-11 22:45:48 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Maze_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.ColumnCount = 30;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Margin = new System.Windows.Forms.Padding(0);
+            this.table.Name = "table";
+            this.table.RowCount = 30;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.Size = new System.Drawing.Size(600, 600);
+            this.table.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.table);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(608, 627);
+            this.MinimumSize = new System.Drawing.Size(608, 627);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Maze Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel table;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze_Generator
+{
+    public partial class Form1 : Form
+    {
+        private MyPanel[,] panels;
+        private int rows, cols;
+        private MyPanel cur;
+        private int totalVisited = 0;
+        private Stack<MyPanel> stack;
+        List<MyPanel> n = new List<MyPanel>();
+        public Form1()
+        {
+            InitializeComponent();
+            rows = table.RowCount;
+            cols = table.ColumnCount;
+            Setup();
+        }
+
+        private void Setup()
+        {
+            stack = new Stack<MyPanel>();
+            panels = new MyPanel[rows,cols];
+            for (int i = 0; i<cols; i++)
+            {
+                for(int j = 0; j<rows; j++)
+                {
+                    panels[i, j] = new MyPanel();
+                    panels[i,j].Size = new Size(table.Height / table.ColumnCount, table.Width / table.RowCount);
+                    panels[i, j].BackColor = Color.Black;
+                    table.Controls.Add(panels[i, j]);
+                    table.SetCellPosition(panels[i, j], new TableLayoutPanelCellPosition(i, j));
+                    panels[i, j].Margin = new Padding(1);
+                    panels[i, j].col = i;
+                    panels[i, j].row = j;
+
+                }
+            }
+            cur = panels[0, 0];
+            cur.visit();
+            totalVisited++;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cur.BackColor = Color.Green;
+            if (totalVisited < cols * rows)
+            {
+                Neighbours();
+                if (n.Count > 0)
+                {
+                    Random rnd = new Random();
+                    int r = rnd.Next(n.Count);
+                    MyPanel chosen = n[r];
+                    stack.Push(cur);
+                    DeleteWall(chosen);
+                    cur = chosen;
+                    cur.visit();
+                    totalVisited++;
+                }
+                else
+                {
+                    cur = stack.Pop();
+                    cur.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void DeleteWall(MyPanel p)
+        {
+            if (cur.col == p.col)
+            {
+                if (cur.row < p.row)
+                { 
+                    p.Margin = new Padding(p.Margin.Left, 0, p.Margin.Right, p.Margin.Bottom);
+                    cur.Margin = new Padding(cur.Margin.Left, cur.Margin.Top, cur.Margin.Right, 0);
+                }
+                else
+                {
+                    p.Margin = new Padding(p.Margin.Left, p.Margin.Top, p.Margin.Right, 0);
+                    cur.Margin = new Padding(cur.Margin.Left, 0, cur.Margin.Right, cur.Margin.Bottom);
+                }
+            }
+            else
+            {
+                if (cur.col < p.col)
+                {
+                    p.Margin = new Padding(0, p.Margin.Top, p.Margin.Right, p.Margin.Bottom);
+                    cur.Margin = new Padding(cur.Margin.Left, cur.Margin.Top, 0, cur.Margin.Bottom);
+                }
+                else
+                {
+                    p.Margin = new Padding(p.Margin.Left, p.Margin.Top, 0, p.Margin.Bottom);
+                    cur.Margin = new Padding(0, cur.Margin.Top, cur.Margin.Right, cur.Margin.Bottom);
+                }
+            }
+        }
+
+
+        private void Neighbours()
+        {
+            n.Clear();
+            if(cur.col!=0 && cur.col != cols - 1)
+            {
+                if (!panels[cur.col + 1, cur.row].visited)
+                    n.Add(panels[cur.col + 1, cur.row]);
+                if (!panels[cur.col - 1, cur.row].visited)
+                    n.Add(panels[cur.col - 1, cur.row]);
+            }
+            else
+            {
+                if (cur.col == 0)
+                {
+                    if (!panels[cur.col + 1, cur.row].visited)
+                        n.Add(panels[cur.col + 1, cur.row]);
+                }
+                else
+                {
+                    if (!panels[cur.col - 1, cur.row].visited)
+                        n.Add(panels[cur.col - 1, cur.row]);
+                }
+            }
+            if(cur.row!=0 && cur.row != rows - 1)
+            {
+                if (!panels[cur.col, cur.row+1].visited)
+                    n.Add(panels[cur.col, cur.row+1]);
+                if (!panels[cur.col, cur.row - 1].visited)
+                    n.Add(panels[cur.col, cur.row - 1]);
+            }
+            else
+            {
+                if (cur.row == 0)
+                {
+                    if (!panels[cur.col, cur.row + 1].visited)
+                        n.Add(panels[cur.col, cur.row + 1]);
+                }
+                else
+                {
+                    if (!panels[cur.col, cur.row - 1].visited)
+                        n.Add(panels[cur.col, cur.row - 1]);
+                }
+            }
+        }
+    }
+    public class MyPanel : Panel
+    {
+        public bool visited = false;
+        public int row;
+        public int col;
+        public void visit()
+        {
+            visited = true;
+            this.BackColor = Color.Red;
+        }
+    }
+}
